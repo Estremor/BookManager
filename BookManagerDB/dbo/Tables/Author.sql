d784a1920d3b14b58c6e53a5388bce9ad6567604
Alexextremor <alexextremor@gmail.com>
2020-06-23 00:34:44 -0500

    Modelo de Base de Datos

---
+++ BookManagerDB/dbo/Tables/Author.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Author] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Name]      VARCHAR (300) NOT NULL,
+    [LastName]  VARCHAR (300) NULL,
+    [BirthDate] DATE          NULL,
+    CONSTRAINT [PK_Author] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ BookManagerDB/dbo/Tables/Book.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[Book] (
+    [Id]         INT           IDENTITY (1, 1) NOT NULL,
+    [Name]       VARCHAR (300) NOT NULL,
+    [AuthorId]   INT           NOT NULL,
+    [CategoryId] INT           NOT NULL,
+    [ISBN]       VARCHAR (13)  NOT NULL,
+    CONSTRAINT [PK_Book] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Book_Author] FOREIGN KEY ([AuthorId]) REFERENCES [dbo].[Author] ([Id]),
+    CONSTRAINT [FK_Book_Category] FOREIGN KEY ([CategoryId]) REFERENCES [dbo].[Category] ([Id])
+);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Book_CategoryId]
+    ON [dbo].[Book]([CategoryId] ASC);
+
+
+GO
+CREATE NONCLUSTERED INDEX [IX_Book_AuthorId]
+    ON [dbo].[Book]([AuthorId] ASC);
+
